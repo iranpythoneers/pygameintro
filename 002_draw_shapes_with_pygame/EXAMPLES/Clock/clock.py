a9fec6e00ae5de2b08214c1a1e804751a35a98a4
Mahyar <mr.riazati1999@gmail.com>
2020-10-02 12:49:18 +0330

    Clock

--- 002_draw_shapes_with_pygame/EXAMPLES/Clock/clock.py
+++ 002_draw_shapes_with_pygame/EXAMPLES/Clock/clock.py
@@ -1,29 +1,81 @@
 import time
 import pygame
 import jdatetime
+from math import sin, cos, pi, radians
 
 pygame.init()
 X = Y  = 800
 SIZE   = [X, Y]
 screen = pygame.display.set_mode(SIZE)
-
+BLACK  = [0] * 3
 class Clock:
-    def __init__(self, size, radius):
+    def __init__(self, size, radius, screen, color = [255] * 3 ):
+        self.screen = screen
+        self.color  = color
         self.cx     = size[0] // 2
         self.cy     = size[1] // 2
         self.radius = radius
-        self.pivotH = self.cy + self.radius - self.radius * 3 / 10
-        self.pivotM = self.cy + self.radius - self.radius * 2 / 10
-        self.pivotS = self.cy + self.radius - self.radius / 10
-        self.endH   = self.pivotH
-        self.endM   = self.pivotM
-        self.endS   = self.pivotS
+        
+        self.pivotH = [self.cx, self.cy - self.radius + self.radius / 2]
+        self.pivotM = [self.cx, self.cy - self.radius + self.radius / 10]
+        self.pivotS = [self.cx, self.cy - self.radius + self.radius / 30]
+
+        self.endH   = None
+        self.endM   = None
+        self.endS   = None
+       
+        self.setEndPoitns()
 
     def getTime(self):
-        return jdatetime.time.now()
+        return jdatetime.datetime.now()
     
     def setEndPoitns(self):
         currentTime = self.getTime()
-        thetaM = None
-        thetaH = None
-        thetaS = None
+        
+        hours  = currentTime.hour
+        hours  = hours if hours < 13 else hours - 12
+        minute = currentTime.minute
+        second = currentTime.second
+
+        thetaH = 360 * hours  / 12
+        thetaM = 360 * minute / 60
+        thetaS = 360 * second / 60
+
+
+        self.endH = self.rotate(self.pivotH, thetaH)
+        self.endM = self.rotate(self.pivotM, thetaM)
+        self.endS = self.rotate(self.pivotS, thetaS)
+    
+    def rotate(self, pos, degree):
+        theta = radians(degree)
+        sinT  = sin(theta)
+        cosT  = cos(theta)
+        px    = pos[0] - self.cx
+        py    = pos[1] - self.cy
+        newX  = px * cosT - py * sinT + self.cx
+        newY  = py * cosT + px * sinT + self.cy
+        return [newX, newY]
+    
+    def drawClock(self):
+        scr  = self.screen
+        clr  = self.color
+        cntr = [self.cx, self.cy]
+        r    = self.radius 
+        # Draw Clock Circle
+        pygame.draw.circle(scr, clr, cntr, r, 1)
+        # Draw Hour Hand
+        pygame.draw.line(scr, clr, cntr, self.endH)
+        # Draw Minute Hand
+        pygame.draw.line(scr, clr, cntr, self.endM)
+        # Draw Hour Second
+        pygame.draw.line(scr, clr, cntr, self.endS)
+        # Update End Points
+        self.setEndPoitns()
+
+myColock = Clock(SIZE, 200, screen)
+
+while True:
+    screen.fill(BLACK)
+    myColock.drawClock()
+    pygame.time.delay(1)
+    pygame.display.update()
